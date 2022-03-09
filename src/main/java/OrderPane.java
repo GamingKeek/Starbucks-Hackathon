e5b5a669a7739bec6c5835c44b15c88af5045bf5
Kelly Raines <82291815+GamingKeek@users.noreply.github.com>
2022-03-09 15:41:58 -0700

    Update OrderPane.java
    changed the buttons at the bottom and test prints of button functions

--- src/main/java/OrderPane.java
+++ src/main/java/OrderPane.java
@@ -1,36 +1,23 @@
-import javafx.scene.layout.*;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javafx.scene.control.Button;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import javax.swing.JOptionPane;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 public class OrderPane extends VBox{
-	private ArrayList<String> suggestionList;
+	private ArrayList<Button> suggestionButtons;
 	private String translatedSentence = "I want a tall hot coffee with milk";
 	private HBox topHBox;
 	private HBox bottomHBox;
@@ -45,19 +32,17 @@ public class OrderPane extends VBox{
 	private Label customerTranslation;
 	private Label phoneticTranslation;
 	private Button audioButton;
-	private Button suggestion1;
-	private Button suggestion2;
-	private Button suggestion3;
 	private Label liveTranslationLabel;
 	private TextArea liveTranslationTextArea;
 	private String audioFile;
 	private final String[] partnerGreetings = {"Welcome to Starbucks", "Hi, how are you?"};
-	private final String[] customerGreetings = {"Bienvenido a starbucks", "Hola, cómo estás?"};
-	private final String[] phoneticGreetings = {"phonetic here","olaˈkomo ɛsˈtas"};
-	
+	private final String[] customerGreetings = {"Bienvenido a Starbucks", "Hola, cómo estás?"};
+	private final String[] phoneticGreetings = {"bjɛ̃mbeˈniðo a Starbucks","olaˈkomo ɛsˈtas"};
+	private Image img = new Image("C:/Users/kelly/Downloads/1607bd824e0e35d7e88df23a56a24540.png");
+  
 	//constructor
-	public OrderPane()
-	{
+	public OrderPane() {
+		
 		//"Language Detection: Spanish" is placed into HBox
 		topHBox = new HBox();
 		languageDetected = new Label("Language Detected: ");
@@ -74,7 +59,15 @@ public class OrderPane extends VBox{
 		gridPane = new GridPane();
 		partnerLanguageAbbrev = new Label("EN "); 
 		customerLanguageAbbrev = new Label("SP "); //method
-		audioButton = new Button("listen");
+		
+		audioButton = new Button();
+		ImageView view = new ImageView(img);
+		view.setFitHeight(16);
+		view.setPreserveRatio(true);
+		audioButton.setGraphic(view);
+		audioFile = "C:/Users/kelly/Downloads/milkSpanish.wav";
+		audioButton.setOnAction(new ButtonHandler());
+
 		partnerTranslation = new Label();
 		customerTranslation = new Label();
 		phoneticTranslation = new Label();
@@ -90,98 +83,74 @@ public class OrderPane extends VBox{
 		//live translation area
 		liveTranslationLabel = new Label("Live Translation ("+ customerLanguageAbbrev.getText() + "to " + partnerLanguageAbbrev.getText() +")");
 		liveTranslationTextArea = new TextArea(); //method to display text here
+		liveTranslationTextArea.setEditable(false);
 		updateTextArea();
 		
 		//suggestion area
-		//suggestionList = getSuggestions(translatedSentence);
 		suggestions = new Label("Suggestions for Phrases:");
-		suggestion1= new Button("milk");
-		suggestion2= new Button("size");
-		suggestion3= new Button("espresso");
-		
-		//place buttons in HBox
+		suggestionButtons = new ArrayList<Button>();
 		bottomHBox = new HBox();
-		bottomHBox.getChildren().addAll(suggestion1, suggestion2, suggestion3);//, suggestion2, suggestion3);
+		Order newOrder = new Order();
+		newOrder.setDrink();
+		suggestionButtons.add(new Button(newOrder.getCommonTopics().get(0)));
+		suggestionButtons.add(new Button("Menu Recommendations"));
+		suggestionButtons.add(new Button("Shortages"));
+		for (int i = 0; i < suggestionButtons.size(); i++) {
+			suggestionButtons.get(i).setOnAction(new SuggestionHandler());
+			bottomHBox.getChildren().addAll(suggestionButtons.get(i));
+		}
 		
 		//set all components in VBox
 		this.getChildren().addAll(topHBox, greetingsCombo, gridPane, liveTranslationLabel, liveTranslationTextArea, suggestions, bottomHBox);
-	
-		/*
-		audio= new Button("audio"); //speaker icon button hopefully
-		audio.setOnAction(new ButtonHandler());
-		audioFile = "C:\\Users\\Movi\\Documents\\asuStarbucksHackathon\\src\\milkSpanish.wav";
-		//create an object for this instance of what type of milk
-		setCenter(audio);
-		*/
 	}
 	
-	private class GreetingHandler implements EventHandler<ActionEvent>
-	{
-		public void handle(ActionEvent event)
-		{
+	private class GreetingHandler implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent event) {
 			partnerTranslation.setText(greetingsCombo.getValue());
 			customerTranslation.setText(customerGreetings[Arrays.asList(partnerGreetings).indexOf(greetingsCombo.getValue())]);
 			phoneticTranslation.setText(phoneticGreetings[Arrays.asList(partnerGreetings).indexOf(greetingsCombo.getValue())]);			
 		}
 	}
 	
-	private class ButtonHandler implements EventHandler<ActionEvent> 
-	{
-		public void handle(ActionEvent event) 
-		{
-			if(audioButton.isArmed())
-			{
-				playTranslation(audioFile);
+	private class ButtonHandler implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent event) {
+			playTranslation(audioFile);
+		}
+	}
+
+	private class SuggestionHandler implements EventHandler<ActionEvent> {
+		public void handle(ActionEvent e) {
+			if (e.getSource() == suggestionButtons.get(0)) {
+				System.out.println("What kind of milk?");
+			}
+			else if (e.getSource() == suggestionButtons.get(1)) {
+				System.out.println("I reccommend the Mocha Cookie Crumble Frappuccino");
+			}
+			else {
+				System.out.println("Sorry, we're out of...");
 			}
 		}
 	}
-	/*
-	 private ArrayList<String> getSuggestions(String liveTranslation)
-	    {
-		 	Drink newDrink = new Drink(liveTranslation);
-		    newDrink.setSize();
-	        newDrink.setDrinkTemp();
-	        newDrink.setMilk();
-
-	        ArrayList<String> commonTopics = new ArrayList<String>();
-
-	        if (newDrink.getSize().equals("none")) {
-	            commonTopics.add("Size");
-	        }
-	        
-	        if (newDrink.getDrinkTemp().equals("none")) {
-	            commonTopics.add("Drink Type");
-	        }
-	        if (newDrink.getMilk().equals("none")) {
-	            commonTopics.add("Milk");
-	        }
-	        
-	        return commonTopics;
-	    }
-	 */
 	
-	private void updateTextArea()
-	{
+	private void updateTextArea() {
 		liveTranslationTextArea.setText(translatedSentence);
 	}
 	
-	private void playTranslation(String filename)
-	{
-		InputStream translation;
-		try
-		{
-			translation = new FileInputStream(new File(filename));
-			AudioStream audio = new AudioStream(translation);
-			AudioPlayer.player.start(audio);
+	private void playTranslation(String translationLocation) {
+		try {
+			File translation = new File(translationLocation);
+			if (translation.exists()) {
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(translation);
+				Clip clip = AudioSystem.getClip();
+				clip.open(audioInput);
+				clip.start();
+			}
+			else {
+				System.out.print("Can't find file");
+			}
 		}
-		catch(Exception e)
-		{
-			JOptionPane.showMessageDialog(null, "Error");
-			
+		catch(Exception e) {
+			e.printStackTrace();	
 		}	
 	}
-	
-
 }
-
-
