358b87125e63fb488c4a2b1525e73333acc23821
mgunatil <93846023+mgunatil@users.noreply.github.com>
2022-03-10 15:30:35 -0700

    Add files via upload
    create a translation object, then call the methods, for example:
    
    Translation translation = new Translation();
    System.out.println(translation.getSpanishTranslation());

---
+++ src/main/java/Translation.java
@@ -0,0 +1,13 @@
+public class Translation {
+	public Translation() {}	
+	
+	public String getSpanishTranslation()
+	{
+		return "Hello my name is Claudia";
+	}
+
+	public String getGermanTranslation()
+	{
+		return "An iced vanilla latte please";
+	}
+}
