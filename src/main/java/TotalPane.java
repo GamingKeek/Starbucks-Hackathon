abb68db451578fe50fd7339fd54af22b1a9c5b36
Kelly Raines <82291815+GamingKeek@users.noreply.github.com>
2022-03-09 18:36:48 -0700

    Add files via upload
    finished totalpane

--- src/main/java/TotalPane.java
+++ src/main/java/TotalPane.java
@@ -1,14 +1,13 @@
 import java.io.File;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -31,12 +30,23 @@ public class TotalPane extends VBox {
     private Label milkPrice;
     private Label orderVerification;
     private Label partnerQuestion;
-    private Label customerResponse;
+    private Label customerQuestion;
     private HBox languageInfo;
     private Label phoeneticResponse;
     private Button responseListen;
     private String audioFile;
     private Image img = new Image("C:/Users/kelly/Downloads/1607bd824e0e35d7e88df23a56a24540.png");
+    private Label totalConfirmation;
+    private Label partnerTotal;
+    private Label customerTotal;
+    private HBox languageTotal;
+    private Label phoeneticTotal;
+    private Button listenTotal;
+    private String audioFile2;
+    private Image img2 = new Image("C:/Users/kelly/Downloads/1607bd824e0e35d7e88df23a56a24540.png");
+    private Label liveTranslationLabel;
+	private TextArea liveTranslationTextArea;
+    private String translatedSentence = "Hi I want a hot coffee with milk. Whole milk. No, that's it. Thank you.";
     
     public TotalPane() {
         screen = new VBox();
@@ -79,8 +89,8 @@ public class TotalPane extends VBox {
         partnerQuestion = new Label("EN:\tDo you want anything else?");
         partnerQuestion.setPadding(new Insets(0, 0, 0, 15));
 
-        customerResponse = new Label("SP:\tDesea algo más?");
-        customerResponse.setPadding(new Insets(0, 0, 0, 15));
+        customerQuestion = new Label("SP:\tDesea algo más?");
+        customerQuestion.setPadding(new Insets(0, 0, 0, 15));
         
         languageInfo = new HBox();
         phoeneticResponse = new Label("Deh-seh-ah ahl-goh mahs");
@@ -94,13 +104,51 @@ public class TotalPane extends VBox {
 		responseListen.setOnAction(new ButtonHandler());
         languageInfo.getChildren().addAll(phoeneticResponse, responseListen);
         
-        screen.getChildren().addAll(order, orderGrid, orderVerification, partnerQuestion, customerResponse, languageInfo);
+
+        totalConfirmation = new Label("If no, let customer know total is displayed on screen");
+        totalConfirmation.setFont(Font.font("Ariel", FontWeight.BOLD, 12));
+        totalConfirmation.setPadding(new Insets(10, 0, 0, 10));
+        
+        partnerTotal = new Label("EN:\tThe total is displayed, thank you.");
+        partnerTotal.setPadding(new Insets(0, 0, 0, 15));
+
+        customerTotal = new Label("SP:\tSe muestra el total, gracias.");
+        customerTotal.setPadding(new Insets(0, 0, 0, 15));
+
+        languageTotal = new HBox();
+        phoeneticTotal = new Label("se ˈmwɛstɾa ɛl toˈtal ˈɡɾak");
+        phoeneticTotal.setPadding(new Insets(0, 10, 0, 15));
+        listenTotal = new Button();
+		ImageView view2 = new ImageView(img2);
+		view2.setFitHeight(14);
+		view2.setPreserveRatio(true);
+		listenTotal.setGraphic(view2);
+		audioFile2 = "C:/Users/kelly/Downloads/milkSpanish.wav";
+		listenTotal.setOnAction(new ButtonHandler());
+        languageTotal.getChildren().addAll(phoeneticTotal, listenTotal);
+
+        //live translation area
+		liveTranslationLabel = new Label("Live Translation (EN to SP)");
+		liveTranslationLabel.setPadding(new Insets(10));
+		liveTranslationTextArea = new TextArea(); //method to display text here
+		liveTranslationTextArea.setEditable(false);
+		liveTranslationTextArea.setTranslateX(10);
+		updateTextArea();
+
+        
+        screen.getChildren().addAll(order, orderGrid, orderVerification, partnerQuestion, customerQuestion, languageInfo,
+            totalConfirmation, partnerTotal, customerTotal, languageTotal, liveTranslationLabel, liveTranslationTextArea);
         this.getChildren().add(screen);
     }
 
     private class ButtonHandler implements EventHandler<ActionEvent> {
 		public void handle(ActionEvent event) {
-			playTranslation(audioFile);
+			if (event.getSource() == responseListen) {
+                playTranslation(audioFile);
+            }
+            else if (event.getSource() == listenTotal) {
+                playTranslation(audioFile2);
+            }
 		}
 	}
 
@@ -120,5 +168,9 @@ public class TotalPane extends VBox {
 		catch(Exception e) {
 			e.printStackTrace();	
 		}	
+	}
+    
+    private void updateTextArea() {
+		liveTranslationTextArea.setText(translatedSentence);
 	}
 }
