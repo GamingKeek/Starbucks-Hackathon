f181f966aaee8273b73756081c311de341d5e54e
Claudia Coots <claudia.j.coots@gmail.com>
2022-03-10 21:12:37 -0600

    Add translations

--- src/main/java/speech/SpeechRecognition.java
+++ src/main/java/speech/SpeechRecognition.java
@@ -1,35 +1,47 @@
+package speech;
+import speech.Translate;
 import com.microsoft.cognitiveservices.speech.*;
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-//comment
-
-import static com.microsoft.cognitiveservices.speech.ResultReason.*;
+import static speech.Translate.prettify;
 
 public class SpeechRecognition {
-    private static String YourSubscriptionKey = "25773c8f0dac46439e82ad77deac3e1a";
-    private static String YourServiceRegion = "eastus";
+    private static final String YourSubscriptionKey = "25773c8f0dac46439e82ad77deac3e1a";
+    private static final String YourServiceRegion = "eastus";
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         SpeechConfig speechConfig = SpeechConfig.fromSubscription(YourSubscriptionKey, YourServiceRegion);
-        speechConfig.setSpeechRecognitionLanguage("es-ES");
+        speechConfig.setSpeechRecognitionLanguage("en-US");
         recognizeFromMicrophone(speechConfig);
     }
 
-    public static void recognizeFromMicrophone(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
+    public static void recognizeFromMicrophone (SpeechConfig speechConfig) throws
+            InterruptedException, ExecutionException {
         //To recognize speech from an audio file, use `fromWavFileInput` instead of `fromDefaultMicrophoneInput`:
         //AudioConfig audioConfig = AudioConfig.fromWavFileInput("YourAudioFile.wav");
         AudioConfig audioConfig = AudioConfig.fromDefaultMicrophoneInput();
         SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-        System.out.println("Speak into your microphone.");
+        System.out.println("\nSpeak into your microphone.");
         Future<SpeechRecognitionResult> task = speechRecognizer.recognizeOnceAsync();
         SpeechRecognitionResult speechRecognitionResult = task.get();
 
         switch (speechRecognitionResult.getReason()) {
             case RecognizedSpeech:
-                System.out.println("RECOGNIZED: Text=" + speechRecognitionResult.getText());
+                System.out.println("RECOGNIZED TEXT: " + speechRecognitionResult.getText());
+                //TRANSLATION
+                Translate translateRequest = new Translate();
+                String response = null;
+                try {
+                    response = translateRequest.Post(speechRecognitionResult.getText());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(prettify(response));
                 //exitCode = 0;
                 break;
             case NoMatch:
---
+++ src/main/java/speech/Translate.java
@@ -0,0 +1,62 @@
+package speech;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.squareup.okhttp.*;
+
+import java.io.IOException;
+
+public class Translate {
+    private static final String subscriptionKey = "49712e8679c94a6581a73861cfe148cd";
+
+    // Add your location, also known as region. The default is global.
+    // This is required if using a Cognitive Services resource.
+    private static final String location = "eastus";
+
+    HttpUrl url = new HttpUrl.Builder()
+            .scheme("https")
+            .host("api.cognitive.microsofttranslator.com")
+            .addPathSegment("/translate")
+            .addQueryParameter("api-version", "3.0")
+            .addQueryParameter("from", "en")
+            .addQueryParameter("to", "de")
+            .addQueryParameter("to", "es")
+            .build();
+
+    // Instantiates the OkHttpClient.
+    OkHttpClient client = new OkHttpClient();
+
+    // This function performs a POST request.
+    public String Post(String textToTranslate) throws IOException {
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType,
+                "[{\"Text\": \"" + textToTranslate + "\"}]");
+        Request request = new Request.Builder().url(url).post(body)
+                .addHeader("Ocp-Apim-Subscription-Key", subscriptionKey)
+                .addHeader("Ocp-Apim-Subscription-Region", location)
+                .addHeader("Content-type", "application/json")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+    // This function prettifies the json response.
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(json_text);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Translate translateRequest = new Translate();
+            String response = translateRequest.Post("example");
+            System.out.println(prettify(response));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
